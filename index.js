87a76997fda93a443827837e634fe772818260f6
Arnav Jindal <arnav.jindal7@gmail.com>
2024-02-12 22:11:59 -0500

    sexed ur mother jk

--- index.js
+++ index.js
@@ -33,6 +33,7 @@ let currInd = 1;
     }
   // Now, let's print the URL and title of each page (tab)
   console.log(url)
+  await page.reload();
 
   const setDate = async () => {
     await page.click('#ctl00_MainContent_BeginRadDateTimePicker_dateInput');
@@ -47,7 +48,10 @@ let currInd = 1;
   console.log("Setting initial date...");
   await setDate();
   console.log("UWU?")
-
+  const element = await page.locator('strong:nth-child(2)');
+  const text = await element.innerText();
+  const target = parseInt(text,10);
+  console.log(`TARGET NO OF PAGES IS ${target}`);
   const innerHTML = await page.innerHTML('#MainContent_ResultPanel');
    allTextItems.push(innerHTML)
    pageIndex++;
@@ -118,7 +122,7 @@ await page.waitForTimeout(7000);
 currInd = 8;
  try {
   while (true) {
-      if (pageIndex >= 54) {
+      if (pageIndex >= 53 ||pageIndex >= target) {
         break;
       }
       if (pageIndex % 10 === 0) {
@@ -147,13 +151,151 @@ currInd = 8;
   }
   pageIndex++;
 }
+  if (pageIndex >= target) {
+    console.log(`[LOG] COMPLETED SCRAPE.`);
+    const filePath = path.join(folderPath, `textItems-${pageIndex / 10}.json`);
+    fs.writeFileSync(filePath, JSON.stringify({data: allTextItems}, null, 2), { encoding: 'utf-8' });
+    await browser.close()
+    return;
+  }
+
 } catch (err) {
   console.log(`[LOG] ERROR OCCURED. SAVING PROGRESS AND QUITTING.`);
   const filePath = path.join(folderPath, `textItems-${pageIndex / 10}.json`);
   fs.writeFileSync(filePath, JSON.stringify({data: allTextItems}, null, 2), { encoding: 'utf-8' });
   return;
 }
+  console.log("[LOG] Scraper must now reload to prevent campus bottlenecking me PART 2 AHHHHH.")
+await page.reload();
+await setDate();
+console.log(`[LOG] SKIPPING TO 11 yay`);
+await page.click(`a:nth-child(11) > span`);
+await page.waitForTimeout(7000);
+console.log(`[LOG] SKIPPING TO 21 because blackjack`);
+await page.click(`a:nth-child(12) > span`);
+await page.waitForTimeout(7000);
+console.log(`[LOG] SKIPPING TO 31 because old man`);
+await page.click(`a:nth-child(12) > span`);
+await page.waitForTimeout(7000);
+console.log(`[LOG] SKIPPING TO 41 because dads`);
+await page.click(`a:nth-child(12) > span`);
+await page.waitForTimeout(7000);
+console.log(`[LOG] SKIPPING TO 51 because ded`);
+await page.click(`a:nth-child(12) > span`);
+await page.waitForTimeout(7000);
+currInd = 4;
+ try {
+  while (true) {
+      if (pageIndex >= 71) {
+        break;
+      }
+      if (pageIndex % 10 === 0) {
+            console.log(`[LOG] WRITING FILE`);
+           const filePath = path.join(folderPath, `textItems-${pageIndex / 10}.json`);
+            fs.writeFileSync(filePath, JSON.stringify({data: allTextItems}, null, 2), { encoding: 'utf-8' });
+            allTextItems.length = 0; // Clear the list for the next batch
+            currInd = 2;
+            console.log(`[LOG] Getting Data for Page ${pageIndex}`);
+            await page.click(`a:nth-child(12) > span`);
+            console.log(`[LOG] TAKING A 20S BREAK TO LET SHIT COOLDOWN ${pageIndex}`);
+            await page.waitForTimeout(20000);
+            const innerHTMLB = await page.innerHTML('#MainContent_ResultPanel');
+            allTextItems.push(innerHTMLB);
+            await page.screenshot({ path: `ss/screenshot_${pageIndex}.png` });
+            currInd++;
+      } else {
 
+          await page.click(`a:nth-child(${currInd}) > span`);
+          await page.waitForTimeout(5000);
+          const innerHTMLB = await page.innerHTML('#MainContent_ResultPanel');
+          allTextItems.push(innerHTMLB);
+          console.log(`[LOG] Getting Data for Page ${pageIndex}`);
+          await page.screenshot({ path: `ss/screenshot_${pageIndex}.png` });
+          currInd++;
+  }
+  pageIndex++;
+}
+if (pageIndex >= target) {
+    console.log(`[LOG] COMPLETED SCRAPE.`);
+    const filePath = path.join(folderPath, `textItems-${pageIndex / 10}.json`);
+    fs.writeFileSync(filePath, JSON.stringify({data: allTextItems}, null, 2), { encoding: 'utf-8' });
+    await browser.close()
+    return;
+  }
+} catch (err) {
+  console.log(`[LOG] ERROR OCCURED. SAVING PROGRESS AND QUITTING.`);
+  const filePath = path.join(folderPath, `textItems-${pageIndex / 10}.json`);
+  fs.writeFileSync(filePath, JSON.stringify({data: allTextItems}, null, 2), { encoding: 'utf-8' });
+  return;
+}
+await page.reload();
+await setDate();
+console.log(`[LOG] SKIPPING TO 11 yay`);
+await page.click(`a:nth-child(11) > span`);
+await page.waitForTimeout(7000);
+console.log(`[LOG] SKIPPING TO 21 because blackjack`);
+await page.click(`a:nth-child(12) > span`);
+await page.waitForTimeout(7000);
+console.log(`[LOG] SKIPPING TO 31 because old man`);
+await page.click(`a:nth-child(12) > span`);
+await page.waitForTimeout(7000);
+console.log(`[LOG] SKIPPING TO 41 because dads`);
+await page.click(`a:nth-child(12) > span`);
+await page.waitForTimeout(7000);
+console.log(`[LOG] SKIPPING TO 51 because ded`);
+await page.click(`a:nth-child(12) > span`);
+await page.waitForTimeout(7000);
+console.log(`[LOG] SKIPPING TO 61 because senior citizens`);
+await page.click(`a:nth-child(12) > span`);
+await page.waitForTimeout(7000);
+console.log(`[LOG] SKIPPING TO 71 because FATASS`);
+await page.click(`a:nth-child(12) > span`);
+await page.waitForTimeout(7000);
+currInd = 3;
+ try {
+  while (true) {
+      if (pageIndex >= 95) {
+        break;
+      }
+      if (pageIndex % 10 === 0) {
+            console.log(`[LOG] WRITING FILE`);
+           const filePath = path.join(folderPath, `textItems-${pageIndex / 10}.json`);
+            fs.writeFileSync(filePath, JSON.stringify({data: allTextItems}, null, 2), { encoding: 'utf-8' });
+            allTextItems.length = 0; // Clear the list for the next batch
+            currInd = 2;
+            console.log(`[LOG] Getting Data for Page ${pageIndex}`);
+            await page.click(`a:nth-child(12) > span`);
+            console.log(`[LOG] TAKING A 20S BREAK TO LET SHIT COOLDOWN ${pageIndex}`);
+            await page.waitForTimeout(20000);
+            const innerHTMLB = await page.innerHTML('#MainContent_ResultPanel');
+            allTextItems.push(innerHTMLB);
+            await page.screenshot({ path: `ss/screenshot_${pageIndex}.png` });
+            currInd++;
+      } else {
+
+          await page.click(`a:nth-child(${currInd}) > span`);
+          await page.waitForTimeout(5000);
+          const innerHTMLB = await page.innerHTML('#MainContent_ResultPanel');
+          allTextItems.push(innerHTMLB);
+          console.log(`[LOG] Getting Data for Page ${pageIndex}`);
+          await page.screenshot({ path: `ss/screenshot_${pageIndex}.png` });
+          currInd++;
+  }
+  pageIndex++;
+}
+if (pageIndex >= target) {
+    console.log(`[LOG] COMPLETED SCRAPE.`);
+    const filePath = path.join(folderPath, `textItems-${pageIndex / 10}.json`);
+    fs.writeFileSync(filePath, JSON.stringify({data: allTextItems}, null, 2), { encoding: 'utf-8' });
+    await browser.close()
+    return;
+  }
+} catch (err) {
+  console.log(`[LOG] ERROR OCCURED. SAVING PROGRESS AND QUITTING.`);
+  const filePath = path.join(folderPath, `textItems-${pageIndex / 10}.json`);
+  fs.writeFileSync(filePath, JSON.stringify({data: allTextItems}, null, 2), { encoding: 'utf-8' });
+  return;
+}
   await browser.close();
   
 })();
